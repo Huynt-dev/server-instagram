f16e733127e26d1ccc46290cd10298725befc582
HuyNT <huynt.me@gmail.com>
2021-03-10 20:49:24 +0700

    ok2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.CORS);
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
