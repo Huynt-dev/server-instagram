8f882f050a53f059610f141278c48df1bcdd1e04
HuyNT <huynt.me@gmail.com>
2021-03-11 15:29:38 +0700

    fix error deploy 4

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const http = require("https");
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.CORS);
-
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, access-control-allow-origin"
   );
   next();
 });
